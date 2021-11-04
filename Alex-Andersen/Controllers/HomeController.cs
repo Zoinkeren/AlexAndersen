fe416d0b239bd07d4e4b105c2fdf70efdd84ab35
Zoinkeren <60384695+Zoinkeren@users.noreply.github.com>
2021-11-04 14:21:41 +0100

    Add project files.

---
+++ Alex-Andersen/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Alex_Andersen.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alex_Andersen.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Messages()
+        {
+            return View();
+        }
+
+        public IActionResult Availability()
+        {
+            return View();
+        }
+
+        public IActionResult Trips()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
