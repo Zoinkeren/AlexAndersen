224e6cb7fc8e7d011909906fc77b058bf4368b18
Zoinkeren <60384695+Zoinkeren@users.noreply.github.com>
2021-12-02 19:59:29 +0100

    expander

--- Alex-Andersen/wwwroot/js/expander.js
+++ Alex-Andersen/wwwroot/js/expander.js
@@ -1,12 +1,12 @@
 ﻿// expander
 
-document.querySelectorAll("expanderButton").forEach(button => {
+document.querySelectorAll('.accordionButton').forEach(button => {
     button.addEventListener('click', () => {
         const expander_Content = button.nextElementSibling;
 
-        button.classList.toggle('expanderButton--active');
+        button.classList.toggle('accordionButton--active');
 
-        if (button.classList.contains('expanderButton--active')) {
+        if (button.classList.contains('accordionButton--active')) {
             expander_Content.style.maxHeight = expander_Content.scrollHeight + 'px';
         } else {
             expander_Content.style.maxHeight = 0;
--- Alex-Andersen/wwwroot/js/site.js
+++ Alex-Andersen/wwwroot/js/site.js
@@ -1,15 +1,1 @@
-﻿// expander
-
-document.querySelectorAll("expanderButton").forEach(button => {
-    button.addEventListener('click', () => {
-        const expander_Content = button.nextElementSibling;
-
-        button.classList.toggle('expanderButton--active');
-
-        if (button.classList.contains('expanderButton--active')) {
-            expander_Content.style.maxHeight = expander_Content.scrollHeight + 'px';
-        } else {
-            expander_Content.style.maxHeight = 0;
-        }
-    });
-});
+﻿
